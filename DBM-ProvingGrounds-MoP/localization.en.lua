229cea6756660ce9a6cca66b950cd6ef3eb9c3d7
Adam <MysticalOS@users.noreply.github.com>
2014-03-11 23:10:53 +0000

    This looks good, now to find someone to lab rat it

--- DBM-ProvingGrounds-MoP/localization.en.lua
+++ DBM-ProvingGrounds-MoP/localization.en.lua
@@ -4,3 +4,8 @@ local L
 -- White TIger Temple --
 ------------------------
 L= DBM:GetModLocalization("d640")
+
+L:SetMiscLocalization({
+	Endless				"Endless",--Could not find a global for this one.
+	ReplyWhisper		= "<Deadly Boss Mods> % is busy in Proving Grounds (Mode: %s Wave: %d)"
+})
