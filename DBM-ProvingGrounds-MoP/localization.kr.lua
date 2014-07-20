8b58caac60e4ae5ab89f36a4196497f314f7fd23
nbluewiz <nbluewiz@e03db02e-9f7d-44a3-8efc-2c2ea4727009>
2014-07-20 03:46:04 +0000

    # koKR update.

--- DBM-ProvingGrounds-MoP/localization.kr.lua
+++ DBM-ProvingGrounds-MoP/localization.kr.lua
@@ -5,3 +5,8 @@ local L
 -- White TIger Temple --
 ------------------------
 L= DBM:GetModLocalization("d640")
+
+L:SetMiscLocalization({
+	Endless				= "무한",
+	ReplyWhisper		= "<Deadly Boss Mods> %s 님은 현재 바쁩니다. 수련의 장을 진행중입니다. (모드: %s, 단계: %d)"
+})
