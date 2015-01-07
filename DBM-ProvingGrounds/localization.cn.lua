a518086ec6db2343c1339b0820dfd9352ebd7b23
ultrashining <ultrashining@e03db02e-9f7d-44a3-8efc-2c2ea4727009>
2015-01-07 07:35:55 +0000

    zhTW/zhCN update

--- DBM-ProvingGrounds/localization.cn.lua
+++ DBM-ProvingGrounds/localization.cn.lua
@@ -5,3 +5,8 @@ local L
 -- White TIger Temple --
 ------------------------
 L= DBM:GetModLocalization("d640")
+
+L:SetMiscLocalization({
+	Endless				= "无尽",--Could not find a global for this one.
+	ReplyWhisper		= "<Deadly Boss Mods> %s正在试炼场试炼中(模式:%s 波数:%d)"
+})
--- DBM-ProvingGrounds/localization.tw.lua
+++ DBM-ProvingGrounds/localization.tw.lua
@@ -5,3 +5,8 @@ local L
 -- White TIger Temple --
 ------------------------
 L= DBM:GetModLocalization("d640")
+
+L:SetMiscLocalization({
+	Endless				= "無盡",--Could not find a global for this one.
+	ReplyWhisper		= "<Deadly Boss Mods> %s正在試煉場試煉中(模式:%s 波數:%d)"
+})
