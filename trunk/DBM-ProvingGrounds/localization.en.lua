fd2332e00cd1a41d6e27a1dd59815e45c8b365fc
Omega <Omega@e03db02e-9f7d-44a3-8efc-2c2ea4727009>
2017-04-04 01:31:19 +0000

    Rename addon to DBM-Challenges

--- trunk/DBM-ProvingGrounds/localization.en.lua
+++ trunk/DBM-ProvingGrounds/localization.en.lua
@@ -9,3 +9,15 @@ L:SetMiscLocalization({
 	Endless				= "Endless",--Could not find a global for this one.
 	ReplyWhisper		= "<Deadly Boss Mods> %s is busy in Proving Grounds (Mode: %s Wave: %d)"
 })
+
+------------------------
+-- Mage Tower --
+------------------------
+L= DBM:GetModLocalization("LegionMageTower")
+
+L:SetGeneralLocalization({
+	name =	"Legion Mage Tower"
+})
+
+L:SetMiscLocalization({
+})
