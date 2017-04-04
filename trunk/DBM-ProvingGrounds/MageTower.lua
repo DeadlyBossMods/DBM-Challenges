3d47004b0487c7e31cfddf415f51887d73d893a2
Omega <Omega@e03db02e-9f7d-44a3-8efc-2c2ea4727009>
2017-04-04 02:48:43 +0000

    First Mage Tower Upddate

--- trunk/DBM-ProvingGrounds/MageTower.lua
+++ trunk/DBM-ProvingGrounds/MageTower.lua
@@ -12,80 +12,145 @@ local L		= mod:GetLocalizedStrings()
 mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetZone()
 
---mod:RegisterCombat("scenario", 1148)
-
---[[
 mod:RegisterEvents(
-	"SPELL_CAST_START",
-	"SPELL_AURA_APPLIED",
-	"SPELL_AURA_APPLIED_DOSE",
+	"SPELL_CAST_START 234423 233473",
+	"SPELL_AURA_APPLIED 234422",
+	"SPELL_AURA_APPLIED_DOSE 234422",
 	"SPELL_CAST_SUCCESS",
 	"UNIT_DIED",
-	"SCENARIO_UPDATE"
+	"UNIT_SPELLCAST_SUCCEEDED boss1 boss2 boss3 boss4 boss5",--need all 5?
+	"INSTANCE_ENCOUNTER_ENGAGE_UNIT"
+--	"SCENARIO_UPDATE"
 )
-Notes: Need EVERYTHING
+--Notes:
+--TODO, all. mapids, mob iDs, win event to stop timers (currently only death event stops them)
 --Tank
--- Variss 177933 does things, but prot warriors can't get through him so didn't last long enough to see what
+-- Stack warning? what amounts switch from reg warning to special warning?
+-- Variss 177933 does things, Only have very little of it. Need more CDs, more warnings
+-- Boss after does things, have no logs of that
+--Healer
+--Damage
 
 --Tank
-local warnWindGuard			= mod:NewSpellAnnounce(144087, 3)
+local warnHolyWard			= mod:NewCastAnnounce(233473, 1)
+local warnDecay				= mod:NewStackAnnounce(234422, 3)
+----Add Spawns
+local warnTormentingEye		= mod:NewSpellAnnounce(234428, 2)
+local warnNetherAberration	= mod:NewSpellAnnounce(235110, 2)
 --Damager
-local warnBanshee			= mod:NewSpellAnnounce(142838, 4)
 --Healer
-local warnStinger			= mod:NewSpellAnnounce(145198, 3)
+
 
 --Tank
-local specWarnPyroBlast		= mod:NewSpecialWarningInterrupt(147601, "HasInterrupt")
+local specWarnDecay			= mod:NewSpecialWarningStack(234422, nil, 5, nil, nil, 1, 6)
+local specWarnDrainLife		= mod:NewSpecialWarningInterrupt(234423)
 --Damager
-local specWarnAmberGlob		= mod:NewSpecialWarningSpell(142189)
 --Healer
-local specWarnStinger		= mod:NewSpecialWarningSpell(145198, false)
+
 
 --Tank
-local timerPowerfulSlamCD	= mod:NewCDTimer(15, 144401, nil, nil, nil, 3)
+local timerDrainLifeCD			= mod:NewAITimer(15, 234423, nil, nil, nil, 4, nil, DBM_CORE_INTERRUPT_ICON)
+local timerHolyWardCD			= mod:NewAITimer(15, 233473, nil, nil, nil, 3, nil, DBM_CORE_HEALER_ICON)
+local timerHolyWard				= mod:NewCastTimer(8, 233473, nil, false, nil, 3, nil, DBM_CORE_HEALER_ICON)
+local timerTormentingEyeCD		= mod:NewAITimer(15, 234428, nil, nil, nil, 1, nil, DBM_CORE_DAMAGE_ICON)
+local timerNetherAbberationCD	= mod:NewAITimer(15, 235110, nil, nil, nil, 1, nil, DBM_CORE_DAMAGE_ICON)
 --Damager
-local timerHealIllusionCD	= mod:NewNextTimer(20, 142238, nil, nil, nil, 4)
 --Healer
-local timerSonicBlastCD		= mod:NewCDTimer(6, 145200, nil, nil, nil, 2)
 
 --local countdownTimer		= mod:NewCountdownFades(10, 141582)
 
-local voiceHealIllusion		= mod:NewVoice(142238)
+local voiceDecay			= mod:NewVoice(234422)--stackhigh
+local voiceDrainLife		= mod:NewVoice(234423)--kickcast
 
 mod:RemoveOption("HealthFrame")
 
 local started = false
+local activeBossGUIDS = {}
 
 function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
-	if spellId == 147601 then
-		warnPyroBlast:Show()
-		specWarnPyroBlast:Show(args.sourceName)
+	if spellId == 234423 then
+		specWarnDrainLife:Show(args.sourceName)
+		voiceDrainLife:Play("kickcast")
+		timerDrainLifeCD:Start()
+	elseif spellId == 233473 then
+		warnHolyWard:Show()
+		timerHolyWard:Start()
+		timerHolyWardCD:Start()
 	end
 end
 
 function mod:SPELL_AURA_APPLIED(args)
 	local spellId = args.spellId
-	if spellId == 144404 then
-
+	if spellId == 234422 then
+		local amount = args.amount or 1
+		if amount >= 5 then
+			specWarnDecay:Show(args.destName)
+			voiceDecay:Play("stackhigh")
+		else
+			warnDecay:Show(args.destName, amount)
+		end
 	end
 end
 mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
 
+--[[
 function mod:SPELL_CAST_SUCCESS(args)
 	local spellId = args.spellId
 	if spellId == 144084 and self:AntiSpam(2, 4) then
-		warnRipperTank:Show()
+
 	end
 end
 
 function mod:UNIT_DIED(args)
 	local cid = self:GetCIDFromGUID(args.destGUID)
-	if cid == 71076 or cid == 71069 then--Illusionary Mystic
-		timerHealIllusionCD:Cancel(args.destGUID)
+	if args.destGUID == UnitGUID("player") then--Solo scenario, a player death is a wipe
+		started = false
+		table.wipe(activeBossGUIDS)
+		timerDrainLifeCD:Stop()
+		timerTormentingEyeCD:Stop()
+		timerHolyWardCD:Stop()
+		timerNetherAbberationCD:Stop()
+	end
+	local cid = self:GetCIDFromGUID(args.destGUID)
+	if cid == 177933 then--Variss
+		timerDrainLifeCD:Stop()
+		timerTormentingEyeCD:Stop()
+		timerNetherAbberationCD:Stop()
+		--Does holy ward reset here? reset timer here if it does
 	end
 end
 
+function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, _, spellGUID)
+	local spellId = tonumber(select(5, strsplit("-", spellGUID)), 10)
+	if spellId == 234428 then--Summon Tormenting Eye
+		warnTormentingEye:Show()
+		timerTormentingEyeCD:Start()--15?
+	elseif spellId == 235110 then--Nether Aberration
+		warnNetherAberration:Show()
+		timerNetherAbberationCD:Start()
+	end
+end
+
+function mod:INSTANCE_ENCOUNTER_ENGAGE_UNIT()
+	for i = 1, 5 do
+		local unitID = "boss"..i
+		local unitGUID = UnitGUID(unitID)
+		if UnitExists(unitID) and not activeBossGUIDS[unitGUID] then
+			local bossName = UnitName(unitID)
+			local cid = self:GetUnitCreatureId(unitID)
+			if cid == 177933 then--Variss
+				started = true
+				timerTormentingEyeCD:Start(1)--3.8?
+				timerHolyWardCD:Start(1)--8?
+				timerDrainLifeCD:Start(1)--9?
+				timerNetherAbberationCD:Start(1)
+			end
+		end
+	end
+end
+
+--[[
 function mod:SCENARIO_UPDATE(newStep)
 	local diffID, currWave, maxWave, duration = C_Scenario.GetProvingGroundsInfo()
 	if diffID > 0 then
