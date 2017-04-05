b6119c8720f1caf8998b778911121f2a6e8349ed
Omega <Omega@e03db02e-9f7d-44a3-8efc-2c2ea4727009>
2017-04-05 11:53:45 +0000

    Add a bunch of stuff for Fel Totem

--- trunk/DBM-Challenges/MageTower.lua
+++ trunk/DBM-Challenges/MageTower.lua
@@ -2,16 +2,19 @@
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
-mod:SetZone()--Healer (1710), Tank (1698), DPS (1703-The God-Queen's Fury)
+mod:SetZone()--Healer (1710), Tank (1698), DPS (1703-The God-Queen's Fury), DPS (Fel Totem Fall)
 
 mod:RegisterEvents(
-	"SPELL_CAST_START 234423 233473",
+	"SPELL_CAST_START 234423 233473 241687 242496 242733",
 	"SPELL_AURA_APPLIED 234422 235984",
 	"SPELL_AURA_APPLIED_DOSE 234422 235833",
-	"SPELL_CAST_SUCCESS",
+	"SPELL_AURA_REMOVED 238471",
+	"SPELL_AURA_REMOVED_DOSE 238471",
+	"SPELL_CAST_SUCCESS 242730 237950",
 	"UNIT_DIED",
 	"UNIT_SPELLCAST_SUCCEEDED boss1 boss2 boss3 boss4 boss5",--need all 5?
-	"INSTANCE_ENCOUNTER_ENGAGE_UNIT"
+	"INSTANCE_ENCOUNTER_ENGAGE_UNIT",
+	"CHAT_MSG_MONSTER_EMOTE"
 --	"SCENARIO_UPDATE"
 )
 --Notes:
@@ -23,28 +26,37 @@ mod:RegisterEvents(
 --Healer
 -- Need ignite soul equiv name/ID.
 -- Need fear name/Id
---Damage
+--Damage (Fel Totem Fall)
+--  ["241687-Sonic Scream"] = "pull:33.8, 48.6, 17.0, 41.3, 15.7, 45.1, 17.0, 43.7, 19.2",
+--  ["242733-Fel Burst"] = "pull:30.2, 23.1, 21.9, 20.7, 19.4, 18.2, 18.2, 23.1, 15.8, 15.8, 14.6, 13.4, 13.4, 12.2, 12.1, 10.9, 12.1, 13.4, 10.9"
 
---Tank
+--Tank (Kruul)
 local warnHolyWard			= mod:NewCastAnnounce(233473, 1)
 local warnDecay				= mod:NewStackAnnounce(234422, 3)
 ----Add Spawns
 local warnTormentingEye		= mod:NewSpellAnnounce(234428, 2)
 local warnNetherAberration	= mod:NewSpellAnnounce(235110, 2)
 local warnInfernal			= mod:NewSpellAnnounce(235112, 2)
---Damager
 --Healer
 local warnArcaneBlitz		= mod:NewStackAnnounce(235833, 2)
+--Damager (fel Totem Fall)
+local warnFelShock			= mod:NewSpellAnnounce(242730, 2, nil, false)
+local warnRupture			= mod:NewSpellAnnounce(241664, 2)
+local warnScale				= mod:NewStackAnnounce(238471, 2)
 
 
 --Tank
 local specWarnDecay			= mod:NewSpecialWarningStack(234422, nil, 5, nil, nil, 1, 6)
 local specWarnDrainLife		= mod:NewSpecialWarningInterrupt(234423)
---Damager
 --Healer
 local specWarnManaSling		= mod:NewSpecialWarningMoveTo(235984, nil, nil, nil, 1, 2)
 local specWarnArcaneBlitz	= mod:NewSpecialWarningStack(235833, nil, 6, nil, nil, 1, 6)--Fine tune the numbers
-
+--Damager (Fel Totem Fall)
+local specWarnSonicScream	= mod:NewSpecialWarningCast(235984, nil, nil, nil, 1, 2)
+local specWarnEarthquake	= mod:NewSpecialWarningSpell(237950, nil, nil, nil, 2, 2)
+local specWarnCharge		= mod:NewSpecialWarningYou(100, nil, nil, nil, 1, 2)--Not real spell ID, but closest match
+local specWarnFelSurge		= mod:NewSpecialWarningSpell(242496, nil, nil, nil, 1, 2)
+local specWarnFelBurst		= mod:NewSpecialWarningSpell(242733, nil, nil, nil, 1, 2)
 
 --Tank
 local timerDrainLifeCD			= mod:NewAITimer(15, 234423, nil, nil, nil, 4, nil, DBM_CORE_INTERRUPT_ICON)
@@ -53,8 +65,12 @@ local timerHolyWard				= mod:NewCastTimer(8, 233473, nil, false, nil, 3, nil, DB
 local timerTormentingEyeCD		= mod:NewAITimer(15, 234428, nil, nil, nil, 1, nil, DBM_CORE_DAMAGE_ICON)
 local timerNetherAbberationCD	= mod:NewAITimer(15, 235110, nil, nil, nil, 1, nil, DBM_CORE_DAMAGE_ICON)
 local timerInfernalCD			= mod:NewAITimer(15, 235112, nil, nil, nil, 1, nil, DBM_CORE_DAMAGE_ICON)
---Damager
 --Healer
+--Damage (fel Totem Fall)
+local timerEarthquakeCD			= mod:NewNextTimer(60, 237950, nil, nil, nil, 2)
+local timerFelSurgeCD			= mod:NewCDTimer(25, 242496, nil, nil, nil, 3)--25-33
+local timerFelRuptureCD			= mod:NewCDTimer(10.9, 241664, nil, nil, nil, 3)--10.9-13.4
+
 
 --local countdownTimer		= mod:NewCountdownFades(10, 141582)
 
@@ -64,6 +80,11 @@ local voiceDrainLife		= mod:NewVoice(234423)--kickcast
 --Healer
 local voiceManaSling		= mod:NewVoice(235984)--findshelter
 local voiceArcaneBlitz		= mod:NewVoice(235833)--stackhigh
+--Damage (Fel Totem Fall)
+local voiceSonicScream		= mod:NewVoice(235984)--stopcast
+local voiceEarthquake		= mod:NewVoice(237950)--aesoon
+local voiceCharge			= mod:NewVoice(100)--chargemove
+local voiceFelSurge			= mod:NewVoice(242496)--stunsoon
 
 mod:RemoveOption("HealthFrame")
 
@@ -80,6 +101,26 @@ function mod:SPELL_CAST_START(args)
 		warnHolyWard:Show()
 		timerHolyWard:Start()
 		timerHolyWardCD:Start()
+	elseif spellId == 158093 then
+		specWarnSonicScream:Show()
+		voiceSonicScream:Play("stopcast")
+	elseif spellId == 242496 then--Fel Surge
+		specWarnFelSurge:Show()
+		voiceFelSurge:Play("stunsoon")
+		timerFelSurgeCD:Start()
+	elseif spellId == 242733 then--Fel Burst (DPS)
+		specWarnFelBurst:Show()
+	end
+end
+
+function mod:SPELL_CAST_SUCCESS(args)
+	local spellId = args.spellId
+	if spellId == 237950 then
+		specWarnEarthquake:Show(args.sourceName)
+		voiceEarthquake:Play("aesoon")
+		timerEarthquakeCD:Start()
+	elseif spellId == 242730 then
+		warnFelShock:Show()
 	end
 end
 
@@ -103,12 +144,21 @@ function mod:SPELL_AURA_APPLIED(args)
 				warnArcaneBlitz:Show(args.destName, amount)
 			end
 		end
-	elseif spellId == 235984 and args:IsPlayer() then
+	elseif spellId == 241687 and args:IsPlayer() then
 		specWarnManaSling:Show(DBM_ALLY)
 		voiceManaSling:Play("findshelter")
 	end
 end
 mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
+
+function mod:SPELL_AURA_REMOVED(args)
+	local spellId = args.spellId
+	if spellId == 238471 then
+		local amount = args.amount or 1
+		warnScale:Show(args.destName, amount)
+	end
+end
+mod.SPELL_AURA_REMOVED_DOSE = mod.SPELL_AURA_REMOVED
 
 --[[
 function mod:SPELL_CAST_SUCCESS(args)
@@ -128,6 +178,9 @@ function mod:UNIT_DIED(args)
 		timerTormentingEyeCD:Stop()
 		timerHolyWardCD:Stop()
 		timerNetherAbberationCD:Stop()
+		timerEarthquakeCD:Stop()
+		timerFelSurgeCD:Stop()
+		timerFelRuptureCD:Stop()
 	end
 	local cid = self:GetCIDFromGUID(args.destGUID)
 	if cid == 177933 then--Variss
@@ -135,6 +188,10 @@ function mod:UNIT_DIED(args)
 		timerTormentingEyeCD:Stop()
 		timerNetherAbberationCD:Stop()
 		--Does holy ward reset here? reset timer here if it does
+	elseif cid == 117230 then--Tugar Bloodtotem (DPS Fel Totem Fall)
+		timerEarthquakeCD:Stop()
+		timerFelSurgeCD:Stop()
+		timerFelRuptureCD:Stop()
 	end
 end
 
@@ -149,6 +206,9 @@ function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, _, spellGUID)
 	elseif spellId == 235112 then--Smoldering Infernal Summon
 		warnInfernal:Show()
 		timerInfernalCD:Start()
+	elseif spellId == 241664 then--Rupture
+		warnRupture:Show()
+		timerFelRuptureCD:Start()
 	end
 end
 
@@ -160,14 +220,27 @@ function mod:INSTANCE_ENCOUNTER_ENGAGE_UNIT()
 			local bossName = UnitName(unitID)
 			local cid = self:GetUnitCreatureId(unitID)
 			--Tank
-			if cid == 177933 then--Variss
+			if cid == 177933 then--Variss (Tank/Kruul Scenario)
 				started = true
 				timerTormentingEyeCD:Start(1)--3.8?
 				timerHolyWardCD:Start(1)--8?
 				timerDrainLifeCD:Start(1)--9?
 				timerNetherAbberationCD:Start(1)
+			elseif cid == 117230 then--Tugar Bloodtotem (DPS Fel Totem Fall)
+				started = true
+				timerFelRuptureCD:Start(7.5)
+				timerEarthquakeCD:Start(20.5)
+				timerFelSurgeCD:Start(62)--Correct place to do it?
 			end
 		end
+	end
+end
+
+--"<53.75 21:03:46> [CHAT_MSG_MONSTER_EMOTE] |TInterface\\Icons\\spell_shaman_earthquake:20|t%s readies itself to charge!#Jormog the Behemoth###Kylistà##0#0##0#12#nil#0#false#false#false#false", -- [133]
+function mod:CHAT_MSG_MONSTER_EMOTE(msg)
+	if msg:find("Interface\\Icons\\spell_shaman_earthquake") then
+		specWarnCharge:Show()
+		voiceCharge:Play("charge")
 	end
 end
 
