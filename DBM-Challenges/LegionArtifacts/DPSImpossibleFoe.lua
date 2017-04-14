7d50bcb0896a005fde45e940f59167dc6a0ce87d
Adam <MysticalOS@users.noreply.github.com>
2017-04-14 07:23:55 +0000

    Drycode a couple warnings/features for imp mother

--- DBM-Challenges/LegionArtifacts/DPSImpossibleFoe.lua
+++ DBM-Challenges/LegionArtifacts/DPSImpossibleFoe.lua
@@ -8,32 +8,48 @@ mod:SetZone()--Healer (1710), Tank (1698), DPS (1703-The God-Queen's Fury), DPS 
 mod:RegisterCombat("combat")
 mod:RegisterEventsInCombat(
 --	"SPELL_CAST_START",
---	"SPELL_AURA_APPLIED",
+	"SPELL_AURA_APPLIED 243113",
 --	"SPELL_AURA_APPLIED_DOSE",
---	"SPELL_AURA_REMOVED",
+	"SPELL_AURA_REMOVED 243113",
 --	"SPELL_AURA_REMOVED_DOSE",
 --	"SPELL_CAST_SUCCESS",
 	"UNIT_DIED",
-	"UNIT_SPELLCAST_SUCCEEDED boss1 boss2 boss3 boss4 boss5",--need all 5?
-	"INSTANCE_ENCOUNTER_ENGAGE_UNIT"
+	"UNIT_SPELLCAST_SUCCEEDED boss1",
 --	"ENCOUNTER_START",
---	"CHAT_MSG_MONSTER_EMOTE"
+	"CHAT_MSG_RAID_BOSS_EMOTE"
 --	"SCENARIO_UPDATE"
 )
 --Notes:
 --TODO, all. mapids, mob iDs, win event to stop timers (currently only death event stops them)
+--NEW VOICE: attackshield
 --Damage
 
 --local warnTormentingEye		= mod:NewSpellAnnounce(234428, 2)
 
 --local specWarnDecay			= mod:NewSpecialWarningStack(234422, nil, 5, nil, nil, 1, 6)
---local specWarnDrainLife		= mod:NewSpecialWarningInterrupt(234423)
+local specWarnImpServants		= mod:NewSpecialWarningSwitch(235140, nil, nil, nil, 1, 2)--Agatha's Vengeance spellId used for now
+local specWarnDarkFury			= mod:NewSpecialWarningSwitch(243111, nil, nil, nil, 1, 7)
 
---local timerDrainLifeCD			= mod:NewAITimer(15, 234423, nil, nil, nil, 4, nil, DBM_CORE_INTERRUPT_ICON)
+local timerImpServantsCD		= mod:NewAITimer(15, 235140, nil, nil, nil, 1)
+local timerDarkFuryCD			= mod:NewAITimer(15, 243111, nil, nil, nil, 5)
 
---local countdownTimer		= mod:NewCountdownFades(10, 141582)
+--local countdownTimer			= mod:NewCountdownFades(10, 141582)
 
---local voiceDecay			= mod:NewVoice(234422)--stackhigh
+local voiceImpServants			= mod:NewVoice(235140)--bigmob
+local voiceDarkFury				= mod:NewVoice(243111)--attackshield/shieldover
+
+mod:AddInfoFrameOption(243113, true)
+
+function mod:OnCombatStart(delay)
+	timerImpServantsCD:Start(1-delay)--14 in one log
+	timerDarkFuryCD:Start(1-delay)
+end
+
+function mod:OnCombatEnd()
+	if self.Options.InfoFrame then
+		DBM.InfoFrame:Hide()
+	end
+end
 
 --[[
 function mod:SPELL_CAST_START(args)
@@ -49,39 +65,32 @@ function mod:SPELL_CAST_SUCCESS(args)
 
 	end
 end
+--]]
 
 function mod:SPELL_AURA_APPLIED(args)
 	local spellId = args.spellId
-	if spellId == 234422 then
-		local amount = args.amount or 1
-		if amount >= 5 then
-			specWarnDecay:Show(amount)
-			voiceDecay:Play("stackhigh")
-		else
-			warnDecay:Show(args.destName, amount)
+	if spellId == 243113 then
+		specWarnDarkFury:Show()
+		voiceDarkFury:Play("attackshield")
+		timerDarkFuryCD:Start()
+		if self.Options.InfoFrame then
+			DBM.InfoFrame:SetHeader(args.spellName)
+			DBM.InfoFrame:Show(2, "enemyabsorb", args.spellName)
 		end
 	end
 end
-mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
+--mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
 
 function mod:SPELL_AURA_REMOVED(args)
 	local spellId = args.spellId
-	if spellId == 238471 then
-		local amount = args.amount or 1
-		warnScale:Show(args.destName, amount)
+	if spellId == 243113 then
+		voiceDarkFury:Play("shieldover")
+		if self.Options.InfoFrame then
+			DBM.InfoFrame:Hide()
+		end
 	end
 end
-mod.SPELL_AURA_REMOVED_DOSE = mod.SPELL_AURA_REMOVED
-
-
-function mod:SPELL_CAST_SUCCESS(args)
-	local spellId = args.spellId
-	if spellId == 144084 and self:AntiSpam(2, 4) then
-
-	end
-end
---]]
-
+--mod.SPELL_AURA_REMOVED_DOSE = mod.SPELL_AURA_REMOVED
 
 function mod:UNIT_DIED(args)
 	if args.destGUID == UnitGUID("player") then--Solo scenario, a player death is a wipe
@@ -130,13 +139,13 @@ function mod:ENCOUNTER_START(id)
 		started = true
 	end
 end
+--]]
 
 --"<53.75 21:03:46> [CHAT_MSG_MONSTER_EMOTE] |TInterface\\Icons\\spell_shaman_earthquake:20|t%s readies itself to charge!#Jormog the Behemoth###Kylistà##0#0##0#12#nil#0#false#false#false#false", -- [133]
-function mod:CHAT_MSG_MONSTER_EMOTE(msg)
-	if msg:find("Interface\\Icons\\spell_shaman_earthquake") then
-		specWarnCharge:Show()
-		voiceCharge:Play("charge")
+function mod:CHAT_MSG_RAID_BOSS_EMOTE(msg)
+	if msg:find(L.impServants) or msg == L.impServants then
+		specWarnImpServants:Show()
+		voiceImpServants:Play("bigmob")
+		timerImpServantsCD:Start()
 	end
 end
-
---]]
