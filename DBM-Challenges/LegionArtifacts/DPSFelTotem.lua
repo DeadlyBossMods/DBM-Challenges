1517d23de1884855c077806eb10e8a0e18e5caf3
Adam <MysticalOS@users.noreply.github.com>
2017-04-15 08:14:54 +0000

    Better handling of status whispers for challenges

--- DBM-Challenges/LegionArtifacts/DPSFelTotem.lua
+++ DBM-Challenges/LegionArtifacts/DPSFelTotem.lua
@@ -5,6 +5,7 @@ mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetCreatureID(117230, 117484)--Tugar, Jormog
 mod:SetZone()--Healer (1710), Tank (1698), DPS (1703-The God-Queen's Fury), DPS (Fel Totem Fall)
 mod:SetBossHPInfoToHighest()
+mod.soloChallenge = true
 
 mod:RegisterCombat("combat")
 mod:RegisterEventsInCombat(
