74f932aef9720c76793ebb277cf34362c069a16a
Adam <MysticalOS@users.noreply.github.com>
2017-05-19 22:57:42 +0000

    Add a somewhat accurate/useful sequence timer for fel burst. it’ll still be wrong in some variances but for most part provide adequate approximation

--- DBM-Challenges/LegionArtifacts/DPSFelTotem.lua
+++ DBM-Challenges/LegionArtifacts/DPSFelTotem.lua
@@ -37,6 +37,7 @@ local specWarnFelBurst		= mod:NewSpecialWarningSpell(242733, nil, nil, nil, 1, 2
 local timerEarthquakeCD		= mod:NewNextTimer(60, 237950, nil, nil, nil, 2)
 local timerFelSurgeCD		= mod:NewCDTimer(25, 242496, nil, nil, nil, 3)--25-33
 local timerFelRuptureCD		= mod:NewCDTimer(10.9, 241664, nil, nil, nil, 3)--10.9-13.4
+local timerFelBurstCD		= mod:NewCDCountTimer(10.9, 242733, nil, nil, nil, 3)--HIGHLY variable
 
 --local countdownTimer		= mod:NewCountdownFades(10, 141582)
 
@@ -45,7 +46,21 @@ local voiceEarthquake		= mod:NewVoice(237950)--aesoon
 local voiceCharge			= mod:NewVoice(100)--chargemove
 local voiceFelSurge			= mod:NewVoice(242496)--stunsoon
 
+--[[
+["242733-Fel Burst"] = "pull:40.2, 23.1, 21.8, 20.7, 21.9, 18.2, 17.7, 19.2, 15.4, 15.8, 14.6, 23.1, 13.3, 12.1, 12.1, 24.3, 10.9, 12.1, 12.1, 19.4",
+["242733-Fel Burst"] = "pull:19.0, 23.1, 21.9, 20.6, 20.7, 19.5, 18.2, 24.3, 15.8, 15.8, 14.6, 17.0, 13.4, 12.1",
+["242733-Fel Burst"] = "pull:31.6, 23.0, 21.9, 21.8, 21.8, 18.2, 18.2, 24.3, 15.8, 17.0, 14.6, 15.8, 13.3, 12.1",
+["242733-Fel Burst"] = "pull:19.5, 23.1, 21.9, 20.7, 19.4, 18.2, 18.2, 29.2, 15.8, 17.0, 26.7, 14.6, 13.3",
+["242733-Fel Burst"] = "pull:22.4, 23.1, 21.9, 20.6, 21.9, 18.2, 19.4, 25.5, 15.8, 17.0, 29.2, 13.4, 14.6, 12.2, 22.3, 11.7, 10.9, 12.1, 24.3",
+["242733-Fel Burst"] = "pull:25.5, 23.1, 21.9, 21.9, 20.7, 18.2, 18.2, 25.5, 15.8, 15.8, 14.6, 15.8, 13.4",
+["242733-Fel Burst"] = "pull:21.4, 23.1, 21.8, 21.8, 20.6, 18.2, 18.2, 24.3, 15.8, 15.8, 14.0, 17.5, 13.3, 12.2, 12.2, 23.1, 13.4, 10.0, 13.1, 26.7, 10.9, 13.3",
+["242733-Fel Burst"] = "pull:30.2, 23.1, 21.9, 20.7, 19.4, 18.2, 18.2, 23.1, 15.8, 15.8, 14.6, 13.4, 13.4, 12.2, 12.1, 10.9, 12.1, 13.4, 10.9",
+--]]
+local felBurstTimers = 	{21.4, 23.1, 21.8, 20.6, 19.4, 18.2, 17.7, 19.2, 15.4, 15.8, 14.0, 13.4, 13.3, 12.2, 12.2, 11.7, 10.9, 10.0, 10.9, 19.4, 10.9, 13.3}
+local felburstCount = 0
+
 function mod:OnCombatStart(delay)
+	felburstCount = 0
 	timerFelRuptureCD:Start(7.5)
 	timerEarthquakeCD:Start(20.5)
 	timerFelSurgeCD:Start(62)--Correct place to do it?
@@ -61,7 +76,12 @@ function mod:SPELL_CAST_START(args)
 		voiceFelSurge:Play("stunsoon")
 		timerFelSurgeCD:Start()
 	elseif spellId == 242733 then--Fel Burst (DPS)
+		felburstCount = felburstCount + 1
 		specWarnFelBurst:Show()
+		local timer = felBurstTimers[felburstCount+1]
+		if timer then
+			timerFelBurstCD:Start(timer, felburstCount+1)
+		end
 	end
 end
 
