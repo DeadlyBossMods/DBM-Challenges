8300059d824c7d7fa461da6d61365ee6725b7bfa
MysticalOS <mysticalosx@gmail.com>
2020-01-15 21:03:17 -0500

    Added chat yell to shockwave on thrall if you are targetted by it and in a group (no yells in solo because that doesn't make sense, who else is he gonna target) Added interrupt warning for Horrifying Shout

--- DBM-Challenges/BfA/NzothVisionOrgrimmar.lua
+++ DBM-Challenges/BfA/NzothVisionOrgrimmar.lua
@@ -11,7 +11,7 @@ mod:RegisterEvents(
 	"ZONE_CHANGED_NEW_AREA"
 )
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_START 297822 297746 304976 297574 304251 306726 299055 299110 307863 300351 300388 304101 304282 306001 306199 303589 305875 306828 306617 300388 296537",
+	"SPELL_CAST_START 297822 297746 304976 297574 304251 306726 299055 299110 307863 300351 300388 304101 304282 306001 306199 303589 305875 306828 306617 300388 296537 305378",
 	"SPELL_AURA_APPLIED 311390 315385 316481 311641",
 	"SPELL_AURA_APPLIED_DOSE 311390",
 	"SPELL_CAST_SUCCESS 297237",
@@ -21,10 +21,7 @@ mod:RegisterEventsInCombat(
 	"ENCOUNTER_START"
 )
 
---TODO, detect engaging of end bosses for start timers
 --TODO, notable trash or affix warnings
---TODO, detect hard modes on end boss
---TODO, verify if scenario is appropriate combat tactic for this mod
 --TODO, maybe add https://ptr.wowhead.com/spell=298510/aqiri-mind-toxin
 --TODO, improve https://ptr.wowhead.com/spell=306001/explosive-leap warning if can get throw target
 --TODO, can https://ptr.wowhead.com/spell=305875/visceral-fluid be dodged? If so upgrade the warning
@@ -49,7 +46,8 @@ local specWarnWaveringWill			= mod:NewSpecialWarningReflect(311641, "false", nil
 --local specWarnHauntingShadows		= mod:NewSpecialWarningDodge(310173, nil, nil, nil, 2, 2)--Not detectable apparently
 --Thrall
 local specWarnSurgingDarkness		= mod:NewSpecialWarningDodge(297822, nil, nil, nil, 2, 2)
-local specWarnSeismicSlam			= mod:NewSpecialWarningDodge(297746, nil, nil, nil, 2, 2)--Can this be dodged?
+local specWarnSeismicSlam			= mod:NewSpecialWarningDodge(297746, nil, nil, nil, 2, 2)
+local yellSeismicSlam				= mod:NewYell(297746)
 --Extra Abilities (used by Thrall and the area LTs)
 local specWarnHopelessness			= mod:NewSpecialWarningMoveTo(297574, nil, nil, nil, 1, 2)
 local specWarnDefiledGround			= mod:NewSpecialWarningDodge(306726, nil, nil, nil, 2, 2)--Can this be dodged?
@@ -66,6 +64,7 @@ local specWarnHowlinginPain			= mod:NewSpecialWarningCast(306199, "SpellCaster",
 local specWarnSanguineResidue		= mod:NewSpecialWarningDodge(303589, nil, nil, nil, 2, 2)
 local specWarnDefiledGround			= mod:NewSpecialWarningDodge(306828, nil, nil, nil, 2, 2)
 local specWarnRingofChaos			= mod:NewSpecialWarningDodge(306617, nil, nil, nil, 2, 2)
+local specWarnHorrifyingShout		= mod:NewSpecialWarningInterrupt(305378, "HasInterrupt", nil, nil, 1, 2)
 local specWarnMentalAssault			= mod:NewSpecialWarningInterrupt(296537, "HasInterrupt", nil, nil, 1, 2)
 
 --Thrall
@@ -81,6 +80,13 @@ local started = false
 local playerName = UnitName("player")
 mod.vb.GnshalCleared = false
 mod.vb.VezokkCleared = false
+
+function mod:SeismicSlamTarget(targetname, uId)
+	if not targetname then return end
+	if targetname == UnitName("player") then
+		yellSeismicSlam:Yell()
+	end
+end
 
 function mod:OnCombatStart(delay)
 	self.vb.GnshalCleared = false
@@ -107,6 +113,9 @@ function mod:SPELL_CAST_START(args)
 		specWarnSeismicSlam:Show()
 		specWarnSeismicSlam:Play("shockwave")
 		timerSeismicSlamCD:Start()
+		if IsInGroup() then
+			self:BossTargetScanner(args.sourceGUID, "SeismicSlamTarget", 0.2, 8)
+		end
 	elseif spellId == 304976 then
 		warnCriesoftheVoid:Show()
 		timerCriesoftheVoidCD:Start()
@@ -157,6 +166,9 @@ function mod:SPELL_CAST_START(args)
 	elseif spellId == 296537 and self:CheckInterruptFilter(args.sourceGUID, false, true) then
 		specWarnMentalAssault:Show(args.sourceName)
 		specWarnMentalAssault:Play("kickcast")
+	elseif spellId == 305378 and self:CheckInterruptFilter(args.sourceGUID, false, true) then
+		specWarnHorrifyingShout:Show(args.sourceName)
+		specWarnHorrifyingShout:Play("kickcast")
 	end
 end
 
