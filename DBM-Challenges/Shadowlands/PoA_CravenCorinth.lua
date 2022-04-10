a1d3db2ce73ac7eb482383af72226521d5c28b5a
MysticalOS <mysticalosx@gmail.com>
2022-04-10 16:13:04 -0400

    cleanup last

---
+++ DBM-Challenges/Shadowlands/PoA_CravenCorinth.lua
@@ -0,0 +1,22 @@
+local mod	= DBM:NewMod("CravenCorinth", "DBM-Challenges", 1)
+--L		= mod:GetLocalizedStrings()
+
+mod:SetRevision("@file-date-integer@")
+mod:SetCreatureID(172412)
+
+mod:RegisterCombat("combat")
+
+mod:RegisterEventsInCombat(
+--	"SPELL_CAST_START",
+--	"SPELL_AURA_APPLIED",
+--	"SPELL_AURA_APPLIED_DOSE",
+--	"SPELL_AURA_REMOVED",
+--	"UNIT_DIED"
+)
+
+local berserkTimer								= mod:NewBerserkTimer(480)
+
+function mod:OnCombatStart(delay)
+	berserkTimer:Start(80-delay)
+end
+
